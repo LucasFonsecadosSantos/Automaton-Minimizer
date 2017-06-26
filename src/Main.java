ab50af153a1fb6836ad78ecdb4730fbc258f54e7
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2017-06-26 23:59:53 +0000

    Directories and JVM compiler bug fixed!

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+/**
+ * AUTOMATON-MINIMIZER 1.0 June 26, 2017
+ * Formal Languages and Atomatons pratical project 2017.
+ * GCCXXX - DCC - Federal University of Lavras.
+ * 
+ * This file the Main implementation class.
+ * 
+ * @author Lucas Fonseca dos Santos      (201712078)
+ * @author Leonardo Carvalho de Oliveira (201420432)
+ * @author Tulio de Oliveira Taveira     (201621232)
+ */
+import utils.Data;
+import java.util.ArrayList;
+
+/**
+ * Main class
+ * This class is responsible to execute the software.
+ * He creates the algorithm objects as: Automaton, data,
+ * tokenizer and others and execute the stages of algorithm.
+ */
+public class Main {
+
+    private static ArrayList<String> data;
+    private ArrayList<String> statesConjunction;
+    private ArrayList<String> finalStatesConjunction;
+    private ArrayList<String> transitionsConjunction;
+    
+    public static void main(String[] args) {
+        Data dt = new Data(args[0], args[1], args[2]);
+        
+        data = new ArrayList<String>();
+        data = dt.returnDescription();
+
+        for(String s : data) {
+            System.out.print(s);
+        }
+
+        /*Tokenizer tk = new Tokenizer();
+
+        statesConjunction = tk.returnStates();
+        finalStatesConjunction = tk.returnFinalStates();
+        transitionsConjunctions = tk.returnTransitions();*/
+
+    }
+
+}
---
+++ src/utils/Data.java
@@ -0,0 +1,81 @@
+/**
+ * AUTOMATON-MINIMIZER 1.0 June 26, 2017
+ * Formal Languages and Atomatons pratical project 2017.
+ * GCCXXX - DCC - Federal University of Lavras.
+ * 
+ * This file the Data implementation class.
+ * 
+ * @author Lucas Fonseca dos Santos      (201712078)
+ * @author Leonardo Carvalho de Oliveira (201420432)
+ * @author Tulio de Oliveira Taveira     (201621232)
+ */
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+/**
+ * This class is responsible to manipulate the data files inputed by user.
+ * He reads the file, recovery the information, returns it and after that,
+ * in the next algorithm stage he writes the result in two others files.
+ */
+public class Data {
+
+    private ArrayList<String> descriptionArray;
+    private BufferedReader inFile;
+    private BufferedWriter outFile_1;
+    private BufferedWriter outFile_2;
+
+    /**
+     * Data object constructor. He receives 3 strings with name and path files.
+     * He creates 3 new objects to read and write this files. If there is an exception,
+     * the method will try with try catch clause.
+     * 
+     * @param descriptionFile Input text file String name/path.
+     * @param tableFile Output text file with a algorithm minimizer table.
+     * @param minimizedAutomaton Output text file with the result of algorithm execution.
+     */
+    public Data(String descriptionFile, String tableFile, String minimizedAutomaton) {
+        descriptionArray = new ArrayList<String>();
+        try {
+            inFile = new BufferedReader(new FileReader(descriptionFile));
+            outFile_1 = new BufferedWriter(new FileWriter(tableFile));
+            outFile_2 = new BufferedWriter(new FileWriter(minimizedAutomaton));
+        }catch(IOException ioex) {
+
+        }
+    }
+
+    /**
+     * This method is reponsible to get the automaton description information
+     * input. He opens the file by BufferedReader object method and verifys the
+     * file validation. After that, he reads line by line and store the lines in
+     * an array list string data structure. This method returns that array.
+     * 
+     * @return ArrayList<String> Java ArrayList data structure that contains all automaton description.
+     */
+    public ArrayList<String> returnDescription() {
+        boolean control = false;
+        String tmpString;
+
+        try {
+            while((tmpString = inFile.readLine()) != null) {
+                if(tmpString == "(") {
+                    control = !control;
+                }
+                if(control) {
+                    descriptionArray.add(tmpString);
+                }
+            }
+        }catch(IOException ioex) {
+
+        }
+        return descriptionArray;
+    }
+
+
+}
