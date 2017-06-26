33f5e94761e17d122d2512bb4aa91ce0bcc264e4
Lucas Fonseca <lucas@lcfcompany.com.br>
2017-06-26 13:35:49 +0000

    First

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ *  AUTOMATON MINIMAZER SOFTWARE
+ *  Formal Languages and Automatons Pratical Project.
+ *  GCCXXX - Federal University of Lavras.
+ *
+ *
+ *  @author Lucas Fonseca dos Santos
+ *  @author Leonardo 
+ */
+
+package src;
+
+import utils.Data;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/utils/Data.java
@@ -0,0 +1,7 @@
+package src.utils;
+
+
+
+public class Data {
+
+}
