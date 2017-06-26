efd6cda676286c398c00e3d6e16ea4f37cf1a57c
Lucas Fonseca <lucas@lcfcompany.com.br>
2017-06-26 18:26:44 +0000

    Deu ruim

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 /**
+ *  @(#) Main.java 1.0 June 26, 2017.
  *  AUTOMATON MINIMAZER SOFTWARE
  *  Formal Languages and Automatons Pratical Project.
  *  GCCXXX - Federal University of Lavras.
  *
  *
- *  @author Lucas Fonseca dos Santos
+ *  @author Lucas Fonseca dos Santos  (201712078)
  *  @author Leonardo 
+ *  @author Tulio de Oliveira Taveira (201621232)
  */
-
 package src;
-
 
 import utils.Data;
 
 public class Main {
 
     public static void main(String[] args) {
-        Data dt = new Data(args);
-        System.out.print("BOSTA");
+        Data dt = new Data();
     }
 
 }
--- src/utils/Data.java
+++ src/utils/Data.java
@@ -1,11 +1,70 @@
+/**
+ *  @(#) Main.java 1.0 June 26, 2017.
+ *  AUTOMATON MINIMAZER SOFTWARE
+ *  Formal Languages and Automatons Pratical Project.
+ *  GCCXXX - Federal University of Lavras.
+ *
+ *
+ *  @author Lucas Fonseca dos Santos  (201712078)
+ *  @author Leonardo 
+ *  @author Tulio de Oliveira Taveira (201621232)
+ */
 package utils;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-
+/**
+ *  This class is responsible to manipulate the input and output data
+ *  file.
+ */
 public class Data {
 
-    public Data(String[] filesName) {
+    /**
+     *  Java BufferedReader object declaration to input file.
+     */
+    private BufferedReader inFile;
+
+    private ArrayList<String> descriptionArray;
+    /**
+     *  Data object constructor. He receives by parameter a string name
+     *  input file to after that, read this file.
+     *
+     *  @param inputFileName String name of input text file.
+     */
+    /*public Data() {
+        try {
+            inFile = new BufferedReader(new FileReader("fire.txt"));
+        }catch(Exception ex) {
+
+        }
+        descriptionArray = new ArrayList<String>();
+    }*/
+
+    /**
+     *  This method is responsible to return all description of automaton
+     *  to the caller method. He reads the input text file, line by line and
+     *  save it in Java Array List data structure.
+     *
+     *  @return ArrayList<String> Array list data structure that contains the automaton description.
+     */
+    public ArrayList<String> returnAutomatonDescription() {
         
+        try{
+            while(inFile.readLine() != null) {
+                try {
+                    descriptionArray.add(inFile.readLine());
+                }catch(IOException ex) {
+                
+                }
+            }
+        }catch(Exception ex) {
+
+        }
+
+        return descriptionArray;
     }
 
 }
